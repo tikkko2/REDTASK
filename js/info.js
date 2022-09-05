aef4101fb154f89ecf19558f720b071ed1c362a1
Tiko <tikoafri2002@gmail.com>
2022-09-05 20:48:34 +0400

    delete comments

--- js/info.js
+++ js/info.js
@@ -66,29 +66,10 @@ function validateEmail(e){
   }
 }
 
-// const api_url = 
-//       "https://pcfy.redberryinternship.ge/api/teams";
-  
-// // Defining async function
-// async function getapi(url) {
-//     const response = await fetch(url);
-//     var data = await response.json();
-//     console.log(data);
-//     if (response) {
-//         hideloader();
-//     }
-//     show(data);
-// }
-// getapi(api_url);
+// const submitNextBtn = document.querySelector(".next-btn");
 
-// function show(data) {
-    
-// }
+// submitNextBtn.addEventListener('submit', function(){
 
-const submitNextBtn = document.querySelector(".next-btn");
-
-submitNextBtn.addEventListener('submit', function(){
-
-});
+// });
 
 
--- js/main.js
+++ js/main.js
@@ -39,39 +39,6 @@ function getSavedValue(v) {
   return localStorage.getItem(v);
 }
 
-// const activePage = window.location.pathname;
-// const navEmployee = document.querySelectorAll('employee a');
-// const navLaptop = document.querySelectorAll('laptop a');
-
-//////////////// ვალიდაცია ინფუთის /////////////////
-
-// function validateEmail(e) {
-//   var inputEmail = document.getElementById("surname").value;
-//   var valid = /^[a-zA-Z]+$/;
-//   if (valid.test(inputEmail) && inputEmail.endsWith("@redberry.ge")) {
-//     $(".email-label").addClass("invalid");
-//     // $(".email").removeClass("blue-border");
-//     $(".email").addClass("invalid-border");
-//     $(".error-email").html(
-//       "<label class='invalid'>გამოიყენე ქართული ასოები</label>"
-//     );
-//     // $(".email-label").removeClass("invalid");
-//     // $(".email").removeClass("invalid-border");
-//     // $(".email").addClass("blue-border");
-//     // $(".error-email").html("<label>მინიმუმ 2 სიმბოლო, ქართული ასოები</label>");
-//   } else {
-//     // $(".email-label").addClass("invalid");
-//     // $(".email").removeClass("blue-border");
-//     // $(".email").addClass("invalid-border");
-//     // $(".error-email").html(
-//     //   "<label class='invalid'>გამოიყენე ქართული ასოები</label>"
-//     // );
-//     $(".email-label").removeClass("invalid");
-//     $(".email").removeClass("invalid-border");
-//     $(".email").addClass("blue-border");
-//     $(".error-email").html("<label>მინიმუმ 2 სიმბოლო, ქართული ასოები</label>");
-//   }
-// }
 
 function validateOnlyNum(e) {
   var inputValue = document.getElementById("phone").value;
@@ -81,61 +48,6 @@ function validateOnlyNum(e) {
 }
 
 //////////////////// get data from API ////////////
-
-// const address = fetch("https://pcfy.redberryinternship.ge/api/teams/")
-//   .then((response) => response.json())
-//   .then((user) => {
-//     return user.address;
-//   });
-
-
-// printAddress();
-
-// let result = {};
-// let allTeam = {};
-
-// async function getTeams() {
-//   try {
-//     let res = await fetch("https://pcfy.redberryinternship.ge/api/teams/");
-//     return await res.json();
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// async function chooseTeam() {
-//   allTeam = await getTeams();
-//   result = Object.assign({}, allTeam);
-//   let select = document.getElementById("team-drop");
-//   for (let i = 0; i < result.data.length; i++) {
-//     let option = document.createElement("a");
-//     option.value = result.data[i]._id;
-//     option.text = result.data[i].name;
-//     select.appendChild(option);
-//   }
-// }
-
-// chooseTeam();
-// const selectEl = document.querySelector("select");
-
-// async function getJSON(){
-//  try{
-//   const res = await fetch('')
-//   return await res.json();
-//  }catch(err){
-//   throw new Error();
-//  }
-// }
-
-// async function generateMarkUp(){
-//  const selectOptionsData = await getJSON()
-//  const optionsMarkUp = selectOptionsData.map(opt=> `<option value=${opt.value}> ${opt.name} </option>`).join('')
-//  selectEl.innerHTML = ''
-//  selectEl.insertAdjacentHTML("afterbegin", optionsMarkUp)
-// }
-
-
-
 
 const api_url = "https://pcfy.redberryinternship.ge/api/teams/";
 async function getTeams(){
